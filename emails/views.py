410c1a5a276c141cb47e8699196acef220ca7134
Tunji Mafe <mafeolatunji17@gmail.com>
2021-03-16 09:09:12 +0100

    New commit

--- emails/views.py
+++ emails/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import EmailEntry
 # Create your views here.
+
+
+def email_entry_get_view(request, *args, **kwargs):
+    obj = EmailEntry.objects.get(id=1)
+    return HttpResponse(f"<h1>Hello {obj.email}</h1>")
+
+
+# def email_entry_list_view():
+#
+#     return
+#
+#
+# def email_entry_create_view():
+#
+#     return
+#
+#
+# def email_entry_update_view():
+#
+#     return
+#
+#
+# def email_entry_destroy_view():
+#
+#     return
